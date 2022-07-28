f43c71f1b753cde44bcf0696f5db586fda75f92e
baristemiz <baristemiz334@gmail.com>
2022-07-28 18:17:03 +0300

    Initial commit

---
+++ src/test/java/baristemiz/getMethod.java
@@ -0,0 +1,99 @@
+package baristemiz;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class getMethod {
+
+    @Test(priority = 1)
+    public void getUserFirstPage(){
+        RestAssured.baseURI="https://reqres.in/api/";
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.get("user?page=1");
+        System.out.println("Sayfa 1: " + response.prettyPrint());
+        System.out.println("Status Code" + ":" + response.statusCode());
+
+        Assert.assertEquals(response.statusCode(),200,"Correct Status");
+
+        if (response.statusCode() == 200){
+            System.out.println("Success");
+        }
+        else {
+            System.out.println("Failed");
+        }
+    }
+
+    @Test(priority = 2)
+    public void getUserSecondPage(){
+        RestAssured.baseURI="https://reqres.in/api/";
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.get("user?page=2");
+        System.out.println("Sayfa 2: " + response.prettyPrint());
+        System.out.println("Status Code" + ":" + response.statusCode());
+    }
+
+    @Test(priority = 3)
+    public void postMethod(){
+        RestAssured.baseURI="https://reqres.in/api/";
+        RequestSpecification httpRequest = RestAssured.given();
+
+        JSONObject reqParams = new JSONObject();
+        reqParams.put("name","Ahmet");
+        reqParams.put("job","QA Tester");
+
+        httpRequest.body(reqParams.toJSONString());
+        Response response = httpRequest.post("users");
+        ResponseBody body = response.getBody();
+
+        System.out.println("Post method Create User: " + response.statusCode());
+        System.out.println("Post method Create User: " + response.prettyPrint());
+    }
+
+    @Test(priority = 4)
+    public void assertMethod(){
+        RestAssured.baseURI="https://reqres.in/api/";
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.get("users/4");
+        JsonPath jsonPath = response.jsonPath();
+
+        String name = jsonPath.get("data.first_name");
+        System.out.println("Name bilgisi: " + name);
+
+        Assert.assertEquals(name,"Eve","Beklediğim user geldi");
+
+        System.out.println("Sayfa 2: " + response.prettyPrint());
+
+        ResponseBody body = response.getBody();
+        String bodyAsString = body.asString();
+        Assert.assertEquals(bodyAsString.contains("Holt"),true);
+
+    }
+
+    @Test(priority = 5)
+    public void postMethod1(){
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+        RequestSpecification httpRequest = RestAssured.given();
+
+        JSONObject reqParams = new JSONObject();
+        reqParams.put("title","fss");
+        reqParams.put("body","dede");
+        reqParams.put("userID","1");
+
+        httpRequest.body(reqParams.toJSONString());
+        Response response = httpRequest.post("posts");
+        ResponseBody body = response.getBody();
+
+        System.out.println("Post method Create User: " + response.statusCode());
+        System.out.println("Post method Create User: " + response.prettyPrint());
+    }
+
+}
